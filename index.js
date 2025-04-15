666b293dbc6cbf235286aecd516bf8982d466477
Andre1753J <andreorocondo7801@gmail.com>
2025-04-15 16:39:05 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from 'cors';
+import multer from "multer";
+
+const app = express();
+
+app.use(cors());
+
+app.post('/login', async (req, res) => {
+    
+})
