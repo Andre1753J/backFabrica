8549563fd0e81de072c99c2cacb33f7bdf1fbf2b
Andre1753J <andreorocondo7801@gmail.com>
2025-04-29 17:04:09 -0400

    conexao

--- services/connection.js
+++ services/connection.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-    port: 20008,
-    host: '200.129.130.149',
+    port: 3306,
+    host: 'ss04ggwskkwc0ksgcookg48w',
     user: 'petsworld',
     password: '12345678',
     database: 'petsworld_db'
