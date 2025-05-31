b541185db22fe892bf4be9fecd295f05586bf204
Andre1753J <andreorocondo7801@gmail.com>
2025-05-31 17:29:20 -0400

    lista todos os animais disponiveis no momento

---
+++ services/listar_A.js
@@ -0,0 +1,23 @@
+import pool from './connection.js';
+
+async function executaQuery(conexao, query, params) {
+    const resposta_query = await conexao.execute(query, params);
+    return resposta_query[0];
+}
+
+export async function listarAnimaisDiponiveis() {
+    const conexao = await pool.getConnection();
+
+    const query = 'SELECT id, nome, idade, sexo, doador FROM animal WHERE disponivel = 1';
+
+    const animais = await executaQuery(conexao, query);
+    conexao.release();
+
+    const animaisFormatados = animais.map(animal => ({
+        ...animal,
+        sexo: animal.sexo === 1 ? 'M' : 'F',
+        disponivel: true
+    }));
+
+    return animaisFormatados;
+}
