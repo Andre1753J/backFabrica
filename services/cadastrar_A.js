b9fbe3fde78b18dd562c4c49a27d6220a79dbaa2
Andre1753J <andreorocondo7801@gmail.com>
2025-05-31 19:11:51 -0400

    ignorar

--- services/cadastrar_A.js
+++ services/cadastrar_A.js
@@ -11,9 +11,7 @@ export async function cadastrar_A(key, nome, idade, sexo, disponivel) {
     const query = 'INSERT INTO animal (nome, idade, sexo, disponivel, doador, adotador) SELECT ?, ?, ?, ?, ?, null FROM cliente WHERE id = ? AND senha = ?';
 
     const keyParts = quebrarKey(key);
-    const id = keyParts[0];
-    const senha = keyParts[2];
-    console.log(keyParts);
+    const [id, , senha] = keyParts;
 
     const retorno = await executaQuery(conexao, query, [nome, idade, sexo, disponivel, id, id, senha]);
     conexao.release();
---
+++ services/dadosAnimais.js
@@ -0,0 +1,26 @@
+export const racaCachorro = [
+    "Akita",
+    "Beagle",
+    "Buldogue Francês",
+    "Chihuahua",
+    "Dálmata",
+    "Golden Retriever",
+    "Labrador Retriever",
+    "Poodle",
+    "Rottweiler",
+    "Shih Tzu",
+    "Outro"
+];
+
+export const racaGato = [
+    ("Abissínio", 2),
+    ("Bengal", 2),
+    ("Birmanês", 2),
+    ("Maine", 2),
+    ("Persa", 2),
+    ("Siamês", 2),
+    ("Sphynx", 2),
+    ("Ragdoll", 2),
+    ("Siberiano", 2),
+    ("Outro", 2)
+];
